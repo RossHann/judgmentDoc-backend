74ee85cec01358b25bc2dfdf8f99fc3304e66b0a
PY <181250111@smail.nju.edu.cn>
2022-03-09 02:35:04 +0800

    first commit

---
+++ src/main/java/com/example/judgmentdoc/JudgmentDocumentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.judgmentdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgmentDocumentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudgmentDocumentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/judgmentdoc/JudgmentDocumentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.judgmentdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgmentDocumentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
