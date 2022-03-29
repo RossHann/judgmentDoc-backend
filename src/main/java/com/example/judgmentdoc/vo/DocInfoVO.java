2c1720b8f13707621b4a76b01bcef0f3905bc5b9
PY <181250111@smail.nju.edu.cn>
2022-03-30 05:42:44 +0800

    feat: 增加DocInfoVO新方法，获取中文日期和成员列表

--- src/main/java/com/example/judgmentdoc/vo/DocInfoVO.java
+++ src/main/java/com/example/judgmentdoc/vo/DocInfoVO.java
@@ -1,5 +1,7 @@
 package com.example.judgmentdoc.vo;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DocInfoVO {
@@ -59,5 +61,79 @@ public class DocInfoVO {
 
     public void setMembers(List<MemberVO> members) {
         this.members = members;
+    }
+
+    public String getChineseDate() {
+        try {
+            String[] dates = this.date.split("-");
+            StringBuilder dateBuilder = new StringBuilder(dates[0] + "年");
+            // 年，直接改就行
+            for (int i = 0; i < 4; i++) {
+                dateBuilder.setCharAt(i, chineseNum(dateBuilder.charAt(i)));
+            }
+            // 月，十、十一、十二前面加十，十月不用加个位
+            if (dates[1].charAt(0) == '1') {
+                dateBuilder.append('十');
+            }
+            if (dates[1].charAt(1) != '0') {
+                dateBuilder.append(chineseNum(dates[1].charAt(1)));
+            }
+            dateBuilder.append('月');
+            // 日
+            if (dates[2].charAt(0) == '2' || dates[2].charAt(0) == '3') {
+                dateBuilder.append(chineseNum(dates[2].charAt(0)) + "十");
+            } else if (dates[2].charAt(0) == '1') {
+                dateBuilder.append("十");
+            }
+            if (dates[2].charAt(1) != '0') {
+                dateBuilder.append(chineseNum(dates[1].charAt(1)));
+            }
+            dateBuilder.append('日');
+            return dateBuilder.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public List<String> getMemberList() {
+        List<String> memberList = new ArrayList<>();
+        Collections.sort(this.members);
+        for (MemberVO member : this.members) {
+            String m = member.getStatus();
+            if (member.getName().length() == 2) {
+                m = m + "　" + member.getName().charAt(0) + "　" + member.getName().charAt(1);
+            } else {
+                m = m + "　" + member.getName();
+            }
+            memberList.add(m);
+        }
+        return memberList;
+    }
+
+    private char chineseNum(char n) {
+        switch (n) {
+            case '0':
+                return '〇';
+            case '1':
+                return '一';
+            case '2':
+                return '二';
+            case '3':
+                return '三';
+            case '4':
+                return '四';
+            case '5':
+                return '五';
+            case '6':
+                return '六';
+            case '7':
+                return '七';
+            case '8':
+                return '八';
+            case '9':
+                return '九';
+            default:
+                return '〇';
+        }
     }
 }
