7676abd1aa969991120dcb09f74c787b08869c16
PY <181250111@smail.nju.edu.cn>
2022-04-02 01:48:33 +0800

    feat: python接口配置

--- src/main/java/com/example/judgmentdoc/bl/model/ModelService.java
+++ src/main/java/com/example/judgmentdoc/bl/model/ModelService.java
@@ -3,12 +3,17 @@ package com.example.judgmentdoc.bl.model;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
-@FeignClient(url = "http://localhost:8000/model", name = "model")
+@FeignClient(url = "${model.url}", name = "${model.name}")
 public interface ModelService {
 
     @PostMapping(value = "/train")
     JSONObject train();
+
+    @GetMapping(value = "/check")
+    JSONObject check(@RequestParam(value = "text") String text, @RequestParam(value = "crime", defaultValue = "traffic") String crime);
 }
